d0294bd9fda0517090aded580c8075c7f69139fb
Salma Ayman <salma.alassal1@gmail.com>
2022-03-16 11:54:53 +0200

    Update Queue.h

--- Queue/Queue.h
+++ Queue/Queue.h
@@ -13,9 +13,7 @@ class Queue
         bool empty();
         int size();
         T front();
-//        void front(T newValue);
         T back();
-//        void back(T newValue);
         void push(T);
         void pop();
         bool full();
@@ -24,15 +22,3 @@ class Queue
 
 
 };
-
-//**empty()**-Returns whether the queue is empty.
-//**size()**-Returns the size of the queue.
-//**front()**-Returns a reference to the first element of the queue.
-//**back()**-Returns a reference to the last element of the queue.
-//**push(x)**-Adds the element 'x' at the end of the queue.
-//**pop()**-Deletes the first element of the queue.
-
-//queue::swap()	Exchange the contents of two queues but the queues must be of the same type, although sizes may differ.
-//queue::emplace()	Insert a new element into the queue container, the new element is added to the end of the queue.
-
-
